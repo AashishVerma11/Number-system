cb7564ed6175d80370a14eab34282897ec6f7cab
Aashish verma <142567347+aashishgithubverma@users.noreply.github.com>
2024-10-27 21:20:22 +0530

    N_number_to_Armstrong_Number

---
+++ N_number_to_Armstrong_Number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int n;
+    
+    scanf("%d",&n);
+    
+    for(int i=100; i<=n; i++){
+        
+        int a = i;
+        int sum = 0;
+        int c =0;
+        
+        for(int b=a; b!=0; b/=10){
+            c++;
+        }
+        
+        for(int b=a; b!=0; b/=10){
+            int rem = b%10;
+            sum = sum + pow(rem,c);
+        }
+        
+        if(sum == i){
+            printf("%d\n",sum);
+        }
+    }
+    
+    return 0;
+}
